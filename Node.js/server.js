9216148a5fc8672b87e39a70ab192c1a2f659ed1
kardiray <kardiray@gmail.com>
2021-09-03 17:05:45 +0900

    MQTT client subscriber

--- Node.js/server.js
+++ Node.js/server.js
@@ -3,17 +3,16 @@ var app = express();
 
 const PORT = 3010;
 
-app.get('/ble', (req, res) => {
-
-})
-
-app.post('/post', (req, res) => {
-    var inputData;
-    req.on('data', (data) => {
-        inputData = JSON.parse(data);
-    })
-})
-
 app.listen(PORT, () => {
     console.log(`Open server on port ${PORT}`);
 })
+
+app.get('/', (req, res) => {
+
+})
+
+app.post('/', (req, res) => {
+    var inputData;
+    console.log(req.body);
+})
+
